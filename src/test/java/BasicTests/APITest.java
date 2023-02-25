6aa62b5ac42dc3a4f9edf068660961e1683a3685
pavankumar.m <pavankumar.m@zohocorp.com>
2023-02-25 22:24:40 +0530

    CC : /Sample API Tests with TestNG annotation and TestNG default report.

---
+++ src/test/java/BasicTests/APITest.java
@@ -0,0 +1,38 @@
+package BasicTests;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class APITest {
+
+    @Test
+    void test1(){
+
+        Response response = get("https://reqres.in/api/users?page=2");
+
+        System.out.println("Status code = "+response.getStatusCode());
+        System.out.println("Response = "+response.asString());
+        System.out.println("Body = "+response.getBody().asString());
+        System.out.println("Time taken = "+response.getTime());
+//        System.out.println("Header = "+response.getHeaders());
+        System.out.println("Header = "+response.getHeader("Content-Type"));
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+//        Assert.assertEquals(statusCode, 201);
+    }
+
+    //BDD driven test
+    @Test
+    void test2(){
+
+        given()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .statusCode(200);
+
+    }
+
+}
